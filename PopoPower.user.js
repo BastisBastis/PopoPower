555e71b3a88adb8521e691ab203dac31f728e280
BastisBastis <sebgus@gmail.com>
2026-02-20 06:47:49 +0100

    v

--- PopoPower.user.js
+++ PopoPower.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            PopoPower
-// @version         0.2.5
+// @version         0.2.6
 // @description     Stora delar skamlöst stulna
 // @match           https://*.popmundo.com/World/Popmundo.aspx/*
 // @require         https://code.jquery.com/jquery-1.7.1.min.js
